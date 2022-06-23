c283030204bb97b8fab89065ee379d80eba2fef7
Ololade0 <adesuyiololade@gmail.com>
2022-06-23 13:44:29 +0100

    my python code

---
+++ Class_Activities/Multiplication.py
@@ -0,0 +1,27 @@
+#for i in range(1,13):
+   # for numbers in range(1,13):
+
+      #  print('{:6} * {: 4} = {:4}' .format(i,numbers, i * numbers))
+
+  #  print()
+
+
+#name = "Java"
+#age = 16
+
+#"print(f'{name =}, {age=}')
+
+#=
+#word = "Hello"
+#for letter in word:
+ #   print(letter)
+
+#def celsius_to_fahrenheit(celsius_float):
+    #"""convert celsius_to_fahrenheit"""
+   # return  celsius_float * 1.8 + 32
+#fahrenheit = celsius_to_fahrenheit(100)
+#print(fahrenheit)
+
+#def cepher_Encripty():
+#primary_key =
+   # return
